702b04765842908ec1e4c8d66e940ff79c151e09
SunriseCoder <sunrisecoder@gmail.com>
2018-08-15 13:46:42 +0300

    Application stub

---
+++ src/main/java/backuper/Backuper.java
@@ -0,0 +1,19 @@
+package backuper;
+
+public class Backuper {
+	private String srcPath;
+	private String dstPath;
+
+	public void setSrcPath(String srcPath) {
+		this.srcPath = srcPath;
+	}
+
+	public void setDstPath(String dstPath) {
+		this.dstPath = dstPath;
+	}
+
+	public void doBackup() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/main/java/backuper/BackuperApp.java
@@ -0,0 +1,23 @@
+package backuper;
+
+public class BackuperApp {
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			printUsage();
+			System.exit(-1);
+		}
+
+		String srcPath = args[0];
+		String dstPath = args[1];
+		
+		Backuper backuper = new Backuper();
+		backuper.setSrcPath(srcPath);
+		backuper.setDstPath(dstPath);
+		backuper.doBackup();
+	}
+
+	private static void printUsage() {
+		System.out.println("Please use: " + BackuperApp.class.getName() + " <source path> <destination path>");
+	}
+}
